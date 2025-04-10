641e5c279c8f3e4f3a548028ecf2e1229fb7ea8a
0Ankit0-0 <ankit.d.vishwakarma@gmail.com>
2025-04-10 17:57:51 +0530

    Update signUp.js

--- frontend/src/Forms/signup/signUp.js
+++ frontend/src/Forms/signup/signUp.js
@@ -69,7 +69,7 @@ const SignUpForm = ({ mode }) => { // Receive 'mode' as a prop
 
         if (validateForm()) {
             try {
-                await axios.post("https://newzzy-apii.vercel.app/api/auth/signup", {
+                await axios.post("https://localhost:5001/api/auth/signup", {
                     username: formData.username,
                     email: formData.email,
                     password: formData.password,
