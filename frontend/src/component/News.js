48896515d5d79f428c17b73b2a83a8a3ec854afc
0Ankit0-0 <ankit.d.vishwakarma@gmail.com>
2025-04-10 18:01:58 +0530

    Update News.js

--- frontend/src/component/News.js
+++ frontend/src/component/News.js
@@ -36,7 +36,7 @@ export class News extends Component {
 
         try {
             this.setState({ loading: true });
-            const url = `http://localhost:5001/api/news?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}`;
+            const url = `https://newzzy-1.onrender.com/api/news?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}`;
             const data = await fetch(url);
             const parsedData = await data.json();
             if (parsedData && Array.isArray(parsedData.articles)) {
